76bd216992fa0541a1850cfb06d06350c214731a
Jesus Lopez <jalopezvillatoro@gmail.com>
2017-07-23 19:47:56 -0600

    agr

--- sortos.js
+++ sortos.js
@@ -1,20 +1,33 @@
 class ToGraph {
   constructor(container, data, type = 'bar3d', width = null, height = null, widthAxe = 30) {
     this.data = data
-    this.axesY = this.getAxesY
-    this.maxAxesY = this.getMaxAxesY
+    this.acumAxesY = this.getAcumAxesY
+    this.allAxesY = this.getAllAxesY
+    this.maxAcumAxesY = this.getMaxAcumAxesY
+    this.maxAllAxesY = this.getMaxAllAxesY
     this.container = container
     this.type = type
-    this.width = width || $(container).css('width')
-    this.height = height || $(container).css('height')
+    this.width = width || parseInt($(container).css('width'))
+    this.height = height || parseInt($(container).css('height'))
     this.widthAxe = widthAxe
+    this.color = [d3.scaleOrdinal()
+                  .range(["#C0392B", "#E74C3C", "#884EA0","#6C3483", "#1F618D","#2874A6", "#148F77", "#117A65", "#1E8449", "#239B56", "#D4AC0D", "#D68910", "#CA6F1E", "#A04000"]),
+                  d3.scaleOrdinal()
+                    .range(["#148F77", "#117A65", "#1E8449", "#239B56", "#D4AC0D", "#D68910", "#CA6F1E", "#A04000"]),
+                  d3.scaleOrdinal()
+                    .range([ "#D4AC0D", "#D68910", "#CA6F1E", "#A04000", "#148F77", "#117A65", "#1E8449", "#239B56"])
+                ]
 
   }
-  get getMaxAxesY () {
-    let axesY = this.axesY
+  get getMaxAllAxesY () {
+    let axesY = this.allAxesY
     return d3.max(axesY)
   }
-  get getAxesY () {
+  get getMaxAcumAxesY () {
+    let axesY = this.acumAxesY
+    return d3.max(axesY)
+  }
+  get getAcumAxesY () {
     let data = this.data
     return data.map(d=>{
       let y = 0
@@ -24,6 +37,16 @@ class ToGraph {
       return y
     })
   }
+  get getAllAxesY () {
+    let data = this.data
+    let y = []
+    let d = data.map(d=>{
+      for(let key in d.axeY) {
+        y.push(parseInt(d.axeY[key]))
+      }
+    })
+    return y
+  }
   createSVG () {
     let container = d3.select(this.container)
     container.selectAll('svg')
@@ -32,6 +55,9 @@ class ToGraph {
                 .append('svg')
                 .attr('width', this.width)
                 .attr('height', this.height)
+                .append('g')
+
+
     this.svg = svg
     this.graphContainer = container
     this.createAxesGraph()
@@ -42,7 +68,10 @@ class ToGraph {
     }
   }
   createBars () {
+
     let h = parseInt(this.height)
+    let w = parseInt(this.width)
+    let max = (Math.round(this.maxAllAxesY / 10) * 10)
     let data = this.data
     let svg = this.svg
     let self = this
@@ -51,9 +80,62 @@ class ToGraph {
                 .enter()
     let it = 0
     let wchart = this.widthAxe
+
+    let axeY = []
+    for (let i = 0; i <= max; i++) {
+      if(i%10 == 0 ) {
+
+        axeY.push(i)
+      }
+    }
+
+    let axes = svg.selectAll('line')
+                  .data(axeY)
+                  .enter().append('line')
+                  .attr('x1',30)
+                  .attr('x2', w)
+                  .style('stroke', 'black')
+                  .style('stroke-width', '1')
+                  .attr('y1', function(d, i){
+                    return Math.round(h - ((h -40)/self.maxAllAxesY * d) -30)
+                  })
+                  .attr('y2', function(d, i){
+
+                    return  Math.round(h - ((h -40)/self.maxAllAxesY * d) -30)
+                  })
+    let axesText = svg.selectAll('text')
+
+    axesText.data(axeY)
+            .enter().append('text')
+            .text(function(d){
+              return d;
+            })
+            .attr('x', 25)
+            .attr('y', function(d, i){
+              return Math.round(h - ((h -40)/self.maxAllAxesY * d) -25)
+            })
+            .style('fill', 'red')
+            .style('text-anchor', 'end')
+    axesText.data(data)
+            .enter().append('text')
+            .text(function(d){
+              return d.axeX;
+            })
+            .attr('x', function(d, i){
+
+              let l = Object.keys(d.axeY).length
+              return (wchart * l  + 40 ) * (i+1) - (20*i) - (wchart * l)
+            })
+            .attr('y', function(d, i){
+              return Math.round(h - 10)
+            })
+            .style('fill', 'red')
+
+
+
     for(let key in bar.datum().axeY) {
       let space = Object.keys(bar.datum().axeY).length
-      let distance = 15 + (wchart*it)
+      let distance = 40 + (wchart*it)
 
       bar.append('rect')
         // .attr('x', 0)
@@ -61,15 +143,33 @@ class ToGraph {
         .attr('width',wchart)
         // .attr('height', 100)
         .attr('x', function(d, i) {
-          return i * wchart * space + distance + (15/2*i)
+          return i * wchart * space + distance + (40/2*i)
         })
         .attr('height', function(d){
-          return (h-40)/self.maxAxesY * d.axeY[key]
+          // return (h-40)/self.maxAxesY * d.axeY[key]
+          return 0
         })
         .attr('y', function(d){
-          return h - ((h-40)/self.maxAxesY * d.axeY[key]) - 20
+          return h - 30
         })
-        .style('fill', `${['red','green'][it]}`)
+        .style('fill', function(d) {
+
+          return self.color[it](d.axeY)
+        })
+        .transition()
+        .delay(function(d, i){return i*50})
+        .duration(500)
+        .attr('height', function(d){
+          return Math.round((h -40)/self.maxAllAxesY * d.axeY[key])
+        })
+        .attr('y', function(d){
+          return Math.round(h - ((h -40)/self.maxAllAxesY * d.axeY[key]) -30)
+        })
+        .style('stroke', function(d) {
+
+          return self.color[it](d.axeY)
+        })
+        .style('stroke-width', '1')
         console.log(it)
         it++
 
